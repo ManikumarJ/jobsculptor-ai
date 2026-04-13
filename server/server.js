afdeea4fd36299863a0bd04809346fd68a2bd478
ManikumarJ <manikumarj6@gmail.com>
2026-04-14 01:58:27 +0530

    Server Updated

--- server/server.js
+++ server/server.js
@@ -1,3 +1,37 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// import express from 'express';
+// import cors from 'cors';
+// import connectDB from './config/db.js';
+
+// import authRoutes from './routes/authRoutes.js';
+// import jobRoutes from './routes/jobRoutes.js';
+// import analyzeRoutes from './routes/resumeRoutes.js'
+// import notificationRoutes from './routes/notificationRoutes.js'
+// import { startCronJobs } from './utils/cronJobs.js';
+
+// connectDB();
+
+// startCronJobs();
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/jobs', jobRoutes);
+// app.use('/api/analyze', analyzeRoutes);
+// app.use('/api/notifications', notificationRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// //  http://localhost:5000/api/analyze/analyze
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,27 +41,34 @@ import connectDB from './config/db.js';
 
 import authRoutes from './routes/authRoutes.js';
 import jobRoutes from './routes/jobRoutes.js';
-import analyzeRoutes from './routes/resumeRoutes.js'
-import notificationRoutes from './routes/notificationRoutes.js'
+import analyzeRoutes from './routes/resumeRoutes.js';
+import notificationRoutes from './routes/notificationRoutes.js';
+
 import { startCronJobs } from './utils/cronJobs.js';
 
 connectDB();
-
 startCronJobs();
 
 const app = express();
 
-// Middleware
-app.use(cors());
 app.use(express.json());
 
-// Routes
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
+app.get("/", (req, res) => {
+    res.send("JobSculptor API is running");
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/analyze', analyzeRoutes);
 app.use('/api/notifications', notificationRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-//  http://localhost:5000/api/analyze/analyze
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
